649d3ebcce8b7b85f8666614c26a2b6c1eda0fca
Mohamed Gaber <me@donn.website>
2026-08-11 15:24:50 +0300

    wheel: install cmake using pip as wheel for full pip install builds

--- wheel_build/before_all.sh
+++ wheel_build/before_all.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-if command -v brew 2> /dev/null; then
-    brew install cmake
-fi
 if command -v yum 2> /dev/null; then
-    yum install -y cmake zlib-devel elfutils-devel elfutils-libelf-devel libdwarf-devel
+    yum install -y zlib-devel elfutils-devel elfutils-libelf-devel libdwarf-devel
 fi
 if command -v apk 2> /dev/null; then
-    apk add cmake libdwarf-dev elfutils-dev zlib-dev
+    apk add libdwarf-dev elfutils-dev zlib-dev
 fi
 cat test/basic.vcd.gz | gunzip > test/basic.vcd
